6f38c42960b54ec39f625ceff422c1ef476e8ab8
josh <postor@gmail.com>
2020-12-02 16:50:25 +0800

    basic

--- docs/next/static/lS5N_iUWektSNAa6wLAZg/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Fpages\u002Findex-b35e78960e0ab6b9ec74.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-7a993814a72fcc4e753f.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
